df68b2b7184ec60450883756fce8fc9f4181fb6d
Nicholas Samaras <n.samaras4@outlook.com>
2024-02-09 12:08:47 -0500

    updates

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App landing" >
+      <h1>New Fresh Redux App</h1>
+    </div>
+  );
+}
+
+export default App;
